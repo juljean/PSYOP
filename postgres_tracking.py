c7ce3e9c35d95deacd41c8a6bdede42ca389a19c
yuliia <yuliia.nad@g-experts.net>
2023-01-15 16:33:38 +0100

    Refactored code, essentials runs are divided one per file

---
+++ postgres_tracking.py
@@ -0,0 +1,20 @@
+import index_prediction
+import time
+import db_connection
+
+
+def query_handler():
+    fetched_data = db_connection.connect(operation_name="select")
+    if fetched_data:
+        # Sort the resultative dictionary by priority
+        fetched_data = sorted(fetched_data.items(), key=lambda x:x[1], reverse=True)
+        # data format [('https://t.me/dvachannel', 0)]
+        for item in fetched_data:
+            index_prediction.get_indexes(item[0])
+            db_connection.connect(channel_link=item[0], operation_name="delete")
+    else:
+        time.sleep(60)
+        query_handler()
+
+
+query_handler()
--- tg_bot.py
+++ tg_bot.py
@@ -1,11 +1,8 @@
-import time
-
 import telebot
 import config
 import pandas as pd
 import uuid
 import db_connection
-import index_prediction
 
 bot = telebot.TeleBot(config.BOT_TOKEN)
 
@@ -29,19 +26,4 @@ def help(message):
     bot.send_message(message.chat.id, 'Введіть /start для того, щоб залишити коментар')
 
 
-def query_handler():
-    fetched_data = db_connection.connect(operation_name="select")
-    if fetched_data:
-        # Sort the resultative dictionary by priority
-        fetched_data = sorted(fetched_data.items(), key=lambda x:x[1], reverse=True)
-        # data format [('https://t.me/dvachannel', 0)]
-        for item in fetched_data:
-            index_prediction.get_indexes(item[0])
-            db_connection.connect(channel_link=item[0], operation_name="delete")
-    else:
-        time.sleep(60)
-        query_handler()
-
-
-query_handler()
 bot.polling()
